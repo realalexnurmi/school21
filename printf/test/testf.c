4bc3f5bc257364cfae00708ab961cdc147f49a9b
Eiji Nena <enena@un-r4.kzn.21-school.ru>
2020-12-03 21:58:54 +0300

    Do some new learning test

--- printf/test/testf.c
+++ printf/test/testf.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
+
+void change(char *s)
+{
+	s[0] = 'Y';
+}
+
 int main()
 {
-	int w = -10;
-	long int x = 5;
-	printf("NO%NO", x);
+	char *s;
+
+	s = calloc(5, sizeof(char));
+
+	s[0] = 'H';
+	s[1] = 'e';
+	s[2] = 'l';
+	s[3] = 'l';
+	change(s);
+	printf("%s",s);
 }
