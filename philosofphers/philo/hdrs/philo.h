f01ff971b58a5bf2a1831bd92552dda2efdbfcd5
Eiji Nena <enena@student.21-school.ru>
2021-10-24 18:38:42 +0300

    op

--- philosofphers/philo/hdrs/philo.h
+++ philosofphers/philo/hdrs/philo.h
@@ -1,7 +1,6 @@
 #ifndef PHILO_H
 # define PHILO_H
 
-# include "libft.h"
 # include <sys/time.h>
 # include <pthread.h>
 
---
+++ philosofphers/philo/srcs/philo.c
@@ -0,0 +1,6 @@
+#include "philo.h"
+
+int main(int argc, char **argv)
+{
+    
+}
